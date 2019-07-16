de1ab81c30e4b38288f0a3a690f8260bf291f433
Damilare <dre4success@live.com>
2019-07-16 04:28:31 +0100

    user controller for device binding

---
+++ src/controllers/user.ts
@@ -0,0 +1,8 @@
+import {Request, Response} from 'express'
+import * as db from '../models'
+
+export const deviceBinding = async (req: Request, res: Response) => {
+  // a device can be binded to five accounts 
+  // and you can have five accounts binded to one device
+  
+}
