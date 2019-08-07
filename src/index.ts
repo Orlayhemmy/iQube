2cc5b35e12f6e6a995af4ca6a553adaf98489a90
dre4success <dre4success@live.com>
2019-08-07 21:51:30 +0100

    change in system

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import * as helmet from 'helmet';
 import * as cors from 'cors';
 import { notFound, prodError } from './errorhandler/errorhandler';
 import { router } from './routes/routes';
+import { guard } from './middleware/gaurd';
 
 dotenv.config({ path: 'application.env' });
 const app = express();
@@ -12,6 +13,7 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(guard);
 app.use('/api', router);
 app.use(notFound);
 app.use(prodError);
