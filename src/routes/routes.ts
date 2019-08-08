8f60befec7d0fe4d2bbc1f936c3dc942f3445db2
dre4success <dre4success@live.com>
2019-08-08 10:10:33 +0100

    loading routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import * as user from '../controllers/user';
 import { asyncError } from '../errorhandler/errorhandler';
+import * as transaction from '../controllers/transactionHistory';
 const router = Router();
 
 router.route('/device/binding').post(asyncError(user.deviceBinding));
-router.route('/login/logs').post(asyncError(user.login))
+router.route('/login/logs').post(asyncError(user.login));
+router.route('/transaction/history').post(asyncError(transaction.sortHistory));
 
 export { router };
