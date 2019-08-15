cc6d44e37f1634f0fa7aa41aaba293d3b9c8d703
dre4success <dre4success@live.com>
2019-08-15 18:02:00 +0100

    fetching all beneficiaries along with their images

--- src/controllers/beneficiary.ts
+++ src/controllers/beneficiary.ts
@@ -4,6 +4,7 @@ import { randomBytes } from 'crypto';
 import { validator } from '../errorhandler/errorhandler';
 import * as db from '../models';
 const editBeneficiaryURl = `https://stanbic.nibse.com/mybank/api/BeneficiaryManagement/EditBeneficiary`;
+const fetchBeneficiaryUrl = `https://stanbic.nibse.com/mybank/api/BeneficiaryManagement/GetBeneficiaryList`;
 
 export const editBeneficiary = async (req: Request, res: Response) => {
   try {
@@ -78,4 +79,57 @@ export const editBeneficiary = async (req: Request, res: Response) => {
   }
 };
 
-export const fetchBeneficiary = async (req: Request, res: Response) => {};
+export const fetchBeneficiary = async (req: Request, res: Response) => {
+  try {
+    let input = ['AccountNo', 'UserId', 'Token'];
+
+    let err = validator(input, req.body);
+    if (err.length) return res.status(400).json({ status: 400, err });
+    const data = {
+      AccountNo: '0023120061',
+      UserId: '0006756856'
+    };
+    const options = {
+      method: 'POST',
+      uri: fetchBeneficiaryUrl,
+      body: data,
+      json: true,
+      headers: {
+        'content-type': 'application/json',
+        'X-STC-AGENT-CACHE': req.body.UserId,
+        Authorization: `Bearer ${req.body.Token}`
+      }
+    };
+    let response = await rp(options);
+    if (response.ResponseCode == '00') {
+      // fetch users with beneficiary id
+      let Beneficiaries: Array<object> = [];
+      for (let val of response.Beneficiaries) {
+        val.image = '';
+        let usersBeneficiaries = await db.Beneficiary.findOne({
+          userId: req.body.UserId,
+          beneficiaryId: val.beneficiaryId
+        });
+
+        if (usersBeneficiaries) val.image = usersBeneficiaries.image;
+
+        Beneficiaries.push(val);
+      }
+
+      return res.status(200).json({
+        Beneficiaries,
+        status: 200,
+        ResponseCode: response.ResponseCode,
+        ResponseFriendlyMessage: response.ResponseFriendlyMessage
+      });
+    }
+    return res.status(400).json({
+      status: response.ResponseCode,
+      message: response.ResponseDescription
+    });
+  } catch (e) {
+    return res
+      .status(e.statusCode)
+      .json({ status: e.statusCode, message: e.message });
+  }
+};
