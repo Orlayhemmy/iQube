a8935feead17d17056429862b36fd7ce313e5b64
dre4success <dre4success@live.com>
2019-08-16 07:16:23 +0100

    increasing payload size

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,8 @@ dotenv.config({ path: 'application.env' });
 const app = express();
 
 app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors());
 app.use(guard);
 app.use('/api', router);
