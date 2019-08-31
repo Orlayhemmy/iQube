12b29ccd11ca81367b938c93bec657a22083d64a
dre4success <dre4success@live.com>
2019-08-31 17:59:36 +0100

    @ease device binding

---
+++ src/controllers/@easesUser.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import * as db from '../models';
+
+export const bindDevice = async (req: Request, res: Response) => {
+
+};
+
+export const login = async (req: Request, res: Response) => {
+  let inputs = ['userID', 'deviceID']
+  
+}
---
+++ src/models/@EaseProfile.ts
@@ -0,0 +1,23 @@
+import { model, Document, Schema } from 'mongoose';
+import { Device } from './Device';
+
+export interface UserAtEase extends Document {
+  userID: string;
+  profilePicture: string;
+  deviceID: string;
+  deviceName: string;
+  deviceOS: string;
+}
+
+const userSchema = new Schema(
+  {
+    userID: { type: String, index: true },
+    profilePicture: String,
+    deviceID: String,
+    deviceName: String,
+    deviceOS: String
+  },
+  { timestamps: true }
+);
+
+export default model<UserAtEase>('UserAtEase', userSchema);
