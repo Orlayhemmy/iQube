f3f9b899fe123b67399f95cb4d87c966fcad3fd7
dre4success <dre4success@live.com>
2019-09-10 10:55:30 +0100

    loading image uploader

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as dotenv from 'dotenv';
 import * as helmet from 'helmet';
 import * as cors from 'cors';
+import * as fileUpload from 'express-fileupload'
 import { notFound, prodError } from './errorhandler/errorhandler';
 import { router } from './routes/routes';
 import { guard } from './middleware/gaurd';
@@ -14,6 +15,7 @@ app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors());
 app.use(guard);
+app.use(fileUpload());
 app.use('/api', router);
 app.use(notFound);
 app.use(prodError);
