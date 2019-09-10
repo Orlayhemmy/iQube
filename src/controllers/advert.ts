8d1e9fcd08a9743bc45175e5dd9d1a64200c52d8
dre4success <dre4success@live.com>
2019-09-10 10:55:48 +0100

    rewriting function to allow image upload

--- src/controllers/advert.ts
+++ src/controllers/advert.ts
@@ -4,10 +4,28 @@ import { validator } from '../errorhandler/errorhandler';
 import request = require('request');
 
 export const createAdvert = async (req: Request, res: Response) => {
-  const input = ['module', 'text', 'title', 'advertImage'];
+  if (!req.files)
+    return res.status(400).json({ status: 400, message: 'No image selected' });
+
+  if (!req.files.advertImage)
+    return res
+      .status(400)
+      .json({ status: 400, message: `Please select an image to upload` });
+
+  if (!req.files.advertImage.mimetype.includes('image/'))
+    return res
+      .status(400)
+      .json({ status: 400, message: `only images allowed for upload` });
+
+  const input = ['module', 'text', 'title'];
+
   let err = validator(input, req.body);
   if (err.length >= 1)
-    return res.status(401).json({ status: 401, message: err });
+    return res.status(400).json({ status: 400, message: err });
+
+  let buf = Buffer.from(req.files.advertImage.data);
+  let base64 = buf.toString('base64');
+
   let modules = ['mybank', 'atease', 'pension', 'mutual', 'insurance'];
 
   if (!modules.includes(req.body.module))
@@ -16,6 +34,7 @@ export const createAdvert = async (req: Request, res: Response) => {
       message: `module can either be mybank, atease, pension, mutual, or insurance`
     });
 
+  req.body.advertImage = base64;
   let advert = await db.Advert.findOne({ module: req.body.module });
   if (!advert) {
     // create advert
