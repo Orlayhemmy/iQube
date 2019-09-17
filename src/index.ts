6d091aa59adc572d760481776bd4cef07b2c6b0a
dre4success <dre4success@live.com>
2019-09-17 15:50:00 +0100

    guarding the endpoints

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as fileUpload from 'express-fileupload';
 import { notFound, prodError } from './errorhandler/errorhandler';
 import { router } from './routes/routes';
 import * as path from 'path';
+import { guard } from './middleware/gaurd';
 
 dotenv.config({ path: 'application.env' });
 const app = express();
@@ -14,11 +15,13 @@ app.use(helmet());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors());
-app.use(fileUpload());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(guard);
+app.use(fileUpload());
 app.use('/api', router);
 app.use(notFound);
 app.use(prodError);
+
 
 const port = process.env.PORT || 5050;
 
