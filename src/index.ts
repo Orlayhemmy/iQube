d45ff86ba9bd00bbd1c784682702256a28135fc1
dre4success <dre4success@live.com>
2019-09-17 15:40:54 +0100

    using static files

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,25 @@ import * as express from 'express';
 import * as dotenv from 'dotenv';
 import * as helmet from 'helmet';
 import * as cors from 'cors';
-import * as fileUpload from 'express-fileupload'
+import * as fileUpload from 'express-fileupload';
 import { notFound, prodError } from './errorhandler/errorhandler';
 import { router } from './routes/routes';
-import { guard } from './middleware/gaurd';
+import * as path from 'path';
 
 dotenv.config({ path: 'application.env' });
 const app = express();
 
 app.use(helmet());
-app.use(express.json({limit: '10mb'}));
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors());
-app.use(guard);
 app.use(fileUpload());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', router);
 app.use(notFound);
 app.use(prodError);
 
-const port = process.env.PORT || 6060;
+const port = process.env.PORT || 5050;
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
