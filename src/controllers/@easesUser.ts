979c9da09374c653d5ec8383dcfc1338422ee584
dre4success <dre4success@live.com>
2019-09-23 22:32:06 +0100

    unlink device

--- src/controllers/@easesUser.ts
+++ src/controllers/@easesUser.ts
@@ -183,3 +183,75 @@ export const login = async (req: Request, res: Response) => {
     return res.status(400).json({ status: 400, message: e.message });
   }
 };
+
+export const unlinkDevice = async (req: Request, res: Response) => {
+  try {
+    // validate otp
+    let inputs = ['deviceID', 'OTP', 'OTPReference', 'UserId'];
+    let err = validator(inputs, req.body);
+    if (err.length >= 1)
+      return res.status(400).json({ status: 400, message: err });
+
+    const data = {
+      UserId: req.body.UserId,
+      OTP: req.body.OTP,
+      OTPReference: req.body.OTPReference
+    };
+    const options = {
+      method: 'POST',
+      uri: validateOTPURL,
+      body: data,
+      json: true,
+      headers: {
+        'content-type': 'application/json'
+      }
+    };
+    let response = await rp(options);
+
+    if (response.ResponseCode == '00') {
+      // find device and update status
+      const user = await db.AtEaseUser.findOne({ userID: req.body.UserId });
+      if (user) {
+        const device = await db.AtEaseDevice.findOne({
+          deviceID: req.body.deviceID,
+          user: user._id,
+          isUnLinked: false
+        });
+        if (!device)
+          return res.status(400).json({
+            status: 400,
+            message: `Could not unlink Device because it is not currently linked`
+          });
+        // unlink device from device account
+        await db.Device.findOneAndUpdate(
+          { _id: device._id },
+          { isUnLinked: true },
+          { new: true }
+        );
+        // unlink device from user's profile
+        await db.AtEaseUser.findOneAndUpdate(
+          { _id: user._id },
+          { $pull: { device: device._id } },
+          { new: true }
+        );
+        return res
+          .status(200)
+          .json({ status: 200, message: 'Device successfully unlinked' });
+      }
+      return res.status(400).json({
+        status: 400,
+        message: `Could not unlink Device because it is not currently linked`
+      });
+    }
+    return res.status(400).json({
+      status: 400,
+      message: `Could not validate OTP`
+    });
+  } catch (e) {
+    if (e.statusCode)
+      return res
+        .status(e.statusCode)
+        .json({ status: e.statusCode, message: e.message });
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+};
