6eb41706c1bac6e4a9bd57e8a2b48f94bccfb0a4
dre4success <dre4success@live.com>
2019-10-07 11:45:49 +0100

    adding FirstName and LastName to user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,13 +5,17 @@ export interface User extends Document {
   userID: string;
   profilePicture: string;
   device: [Device['_id']];
+  FirstName: string;
+  LastName: string;
 }
 
 const userSchema = new Schema(
   {
     userID: { type: String, index: true },
     profilePicture: { type: String, default: '' },
-    device: [{ type: Schema.Types.ObjectId, ref: 'Device' }]
+    device: [{ type: Schema.Types.ObjectId, ref: 'Device' }],
+    FirstName: { type: String, default: '' },
+    LastName: { type: String, default: '' }
   },
   { timestamps: true }
 );
