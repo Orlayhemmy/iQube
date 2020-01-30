f117eed66c665d0d8bfbcf62c206c361a7d706ed
dre4success <dre4success@live.com>
2020-01-30 14:01:13 +0100

    creating log helper

---
+++ src/config/winston.ts
@@ -0,0 +1,41 @@
+import * as winston from 'winston';
+import * as path from 'path';
+
+const logPath = path.join(__dirname, '..', 'logs');
+console.log(logPath)
+
+let options = {
+  file: {
+    level: 'info',
+    filename: path.join(logPath, 'app.log'),
+    handleExceptions: true,
+    format: winston.format.json(),
+    maxsize: 10485760,
+    maxFiles: 5,
+    colorize: winston.format.colorize()
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    )
+  }
+};
+
+let logger = winston.createLogger({
+  transports: [
+    new winston.transports.File(options.file),
+    new winston.transports.Console(options.console)
+  ],
+  exitOnError: false
+});
+
+export const stream = {
+  write(message: any) {
+    logger.info(message);
+  }
+};
+
+export { logger };
