bfb257f0046249e1d4e7b7447aa1f191d5070418
dre4success <dre4success@live.com>
2020-01-30 14:01:53 +0100

    combining logs and initializing

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,10 @@ import { notFound, prodError } from './errorhandler/errorhandler';
 import { router } from './routes/routes';
 import * as path from 'path';
 import { guard } from './middleware/gaurd';
-import * as SwaggerDoc from './openapi.json'
-import * as SwaggerUI from 'swagger-ui-express'
+import * as SwaggerDoc from './openapi.json';
+import * as SwaggerUI from 'swagger-ui-express';
+import * as morgan from 'morgan';
+import { stream } from './config/winston';
 
 dotenv.config({ path: 'application.env' });
 const app = express();
@@ -21,12 +23,13 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(morgan('combined', { stream }));
 app.use(guard);
 app.use(fileUpload());
 app.use('/api', router);
 app.use(notFound);
 app.use(prodError);
-
 
 const port = process.env.PORT || 5050;
 
