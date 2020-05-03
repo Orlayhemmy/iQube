838840c01c5d6a29ee0a51f6678c4a314d8c9196
dre4success <dre4success@live.com>
2020-05-03 15:54:06 +0100

    loading routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,6 +5,7 @@ import * as transaction from '../controllers/transactionHistory';
 import * as beneficiaryController from '../controllers/beneficiary';
 import * as advertController from '../controllers/advert';
 import * as atEaseUser from '../controllers/@easesUser';
+import * as Push from '../controllers/push';
 const router = Router();
 
 router.route('/device/binding').post(asyncError(user.deviceBinding));
@@ -19,32 +20,23 @@ router.route('/atease/device/link').post(asyncError(atEaseUser.bindDevice));
 router.route('/atease/device/unlink').post(asyncError(atEaseUser.unlinkDevice));
 router.route('/atease/device/all').post(asyncError(atEaseUser.viewAllBindedDevices));
 
-router
-  .route('/atease/addprofile')
-  .post(asyncError(atEaseUser.addOrUpdateProfileImage));
+router.route('/atease/addprofile').post(asyncError(atEaseUser.addOrUpdateProfileImage));
 
 router.route('/atease/viewprofile').post(asyncError(atEaseUser.fetchImage));
 
-router
-  .route('/mybank/edit/beneficiary')
-  .post(asyncError(beneficiaryController.editBeneficiary));
-router
-  .route('/mybank/fetch/beneficiary')
-  .post(asyncError(beneficiaryController.fetchBeneficiary));
-router
-  .route('/mybank/add/beneficiary')
-  .post(asyncError(beneficiaryController.addBeneficiary));
+router.route('/mybank/edit/beneficiary').post(asyncError(beneficiaryController.editBeneficiary));
+router.route('/mybank/fetch/beneficiary').post(asyncError(beneficiaryController.fetchBeneficiary));
+router.route('/mybank/add/beneficiary').post(asyncError(beneficiaryController.addBeneficiary));
 
-router
-  .route('/mybank/addprofile')
-  .post(asyncError(user.addOrUpdateProfileImage));
+router.route('/mybank/addprofile').post(asyncError(user.addOrUpdateProfileImage));
 
 router.route('/mybank/viewprofile').post(asyncError(user.fetchImage));
 
-router
-  .route('/admin/advert')
-  .get(asyncError(advertController.fetchAllAdverts));
+router.route('/admin/advert').get(asyncError(advertController.fetchAllAdverts));
 
 router.route('/admin/advert/:id').get(asyncError(advertController.fetchAdvert));
 
+router.route('/push/fetch').post(asyncError(Push.fetchMessages));
+
+router.route('/update/status').post(asyncError(Push.updateStatus));
 export { router };
