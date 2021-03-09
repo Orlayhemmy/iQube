8fc72effd7849839e76944bd49c9d35fd3758cad
Olayemi Lawal <olayemi.lawal@stanbicibtc.com>
2021-03-09 18:17:06 +0100

    add more properties to advert interface

--- src/models/Advert.ts
+++ src/models/Advert.ts
@@ -6,6 +6,9 @@ export interface Advert extends Document {
   text: string;
   link: string;
   title: string;
+  mediaUrl: string;
+  approved: boolean;
+  status: boolean;
 }
 
 const advertSchema: Schema = new Schema(
@@ -15,9 +18,12 @@ const advertSchema: Schema = new Schema(
     text: String,
     link: String,
     title: String,
-    index: Number
+    index: Number,
+    mediaUrl: String,
+    approved: Boolean,
+    status: Boolean
   },
   { timestamps: true }
 );
 
-export default model<Advert>('Advert', advertSchema);
+export default model<Advert>('Adverts', advertSchema);
